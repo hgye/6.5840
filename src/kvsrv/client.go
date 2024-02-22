85cf29564b6d323b0ad12a336149ce6570e7e449
hegel.ye <hegel.ye@calix.com>
2024-02-22 15:14:47 +0800

    lab2 first stage

--- src/kvsrv/client.go
+++ src/kvsrv/client.go
@@ -1,8 +1,12 @@
 package kvsrv
 
-import "6.5840/labrpc"
-import "crypto/rand"
-import "math/big"
+import (
+	"crypto/rand"
+	"fmt"
+	"math/big"
+
+	"6.5840/labrpc"
+)
 
 
 type Clerk struct {
@@ -37,7 +41,22 @@ func MakeClerk(server *labrpc.ClientEnd) *Clerk {
 func (ck *Clerk) Get(key string) string {
 
 	// You will have to modify this function.
-	return ""
+
+	args := GetArgs{
+		Key: key,
+	}
+
+	reply := GetReply{}
+
+	for {
+		ok := ck.server.Call("KVServer.Get", &args, &reply)
+		if !ok {
+			fmt.Errorf("rpc call KVServer.Get failed")
+		} else {
+			break
+		}
+	}
+	return reply.Value
 }
 
 // shared by Put and Append.
@@ -50,7 +69,21 @@ func (ck *Clerk) Get(key string) string {
 // arguments. and reply must be passed as a pointer.
 func (ck *Clerk) PutAppend(key string, value string, op string) string {
 	// You will have to modify this function.
-	return ""
+	args := PutAppendArgs{
+		Key: key,
+		Value: value,
+	}
+	reply := PutAppendReply{}
+
+	for {
+		ok := ck.server.Call("KVServer."+op, &args, &reply)
+		if !ok {
+			fmt.Errorf("rpc call KVServer.%s failed", op)
+		} else {
+			break
+		}
+	}
+	return reply.Value
 }
 
 func (ck *Clerk) Put(key string, value string) {
--- src/kvsrv/server.go
+++ src/kvsrv/server.go
@@ -19,25 +19,48 @@ type KVServer struct {
 	mu sync.Mutex
 
 	// Your definitions here.
+	store map[string]string
 }
 
 
 func (kv *KVServer) Get(args *GetArgs, reply *GetReply) {
 	// Your code here.
+	kv.mu.Lock()
+	v, ok := kv.store[args.Key]
+	kv.mu.Unlock()
+	if !ok {
+		v = ""
+	}
+	reply.Value = v
 }
 
 func (kv *KVServer) Put(args *PutAppendArgs, reply *PutAppendReply) {
 	// Your code here.
+	kv.mu.Lock()
+	kv.store[args.Key] = args.Value
+	kv.mu.Unlock()
+	reply.Value = args.Value
 }
 
 func (kv *KVServer) Append(args *PutAppendArgs, reply *PutAppendReply) {
 	// Your code here.
+	kv.mu.Lock()
+	v, ok := kv.store[args.Key]
+	if !ok {
+		v = ""
+	}
+	kv.store[args.Key] = v+args.Value
+	kv.mu.Unlock()
+
+	reply.Value = v
 }
 
 func StartKVServer() *KVServer {
 	kv := new(KVServer)
 
 	// You may need initialization code here.
+	store := make(map[string]string, 10)
+	kv.store = store
 
 	return kv
 }
